acb85ddf1561a48a1c84c8323b083026a98ed4e2
Nagisa <miku195@mail.ru>
2022-03-07 23:25:15 +0700

    small fix

--- js/html.js
+++ js/html.js
@@ -34,35 +34,5 @@ export function weatherTabs(answer) {
 function weatherNow(answer) {
     nowTemprature.textContent = `${(answer.main.temp - 273.15).toFixed(1)}°`;
     currentCity.textContent = `${answer.name}`;
-    switch (answer.weather[0].main) {
-        case 'Clear': backGround(iconWeather['clear sky']);
-            break;
-        case 'Clouds': backGround(iconWeather['scattered clouds']);
-            break;
-        case 'Drizzle': backGround(iconWeather['shower rain']);
-            break;
-        case 'Rain': backGround(iconWeather['rain']);
-            break;
-        case 'Thunderstorm': backGround(iconWeather['thunderstorm']);
-            break;
-        case 'Snow': backGround(iconWeather['snow']);
-            break;
-        default: backGround(iconWeather['mist']);
-            break;
-    }
+    Now.style = `background: url('http://openweathermap.org/img/wn/${answer["weather"][0]["icon"]}@2x.png') 50% 50%/80px 80px no-repeat`
 }
-
-function backGround(url) {
-    Now.style = `background: url(${url}) 50% 50%/80px 80px no-repeat`;
-}
-
-const iconWeather = {
-    'clear sky': 'http://openweathermap.org/img/wn/01d@2x.png',
-    'scattered clouds': 'http://openweathermap.org/img/wn/03d@2x.png',
-    'shower rain': 'http://openweathermap.org/img/wn/09d@2x.png',
-    'rain': 'http://openweathermap.org/img/wn/10d@2x.png',
-    'thunderstorm': 'http://openweathermap.org/img/wn/11d@2x.png',
-    'snow': 'http://openweathermap.org/img/wn/13d@2x.png',
-    'mist': 'http://openweathermap.org/img/wn/50n@2x.png',
-}
-
